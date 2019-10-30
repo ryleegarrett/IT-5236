20453e93cb48743106dd09caf6ae3cd02867fb0c
ryleegarrett <43964658+ryleegarrett@users.noreply.github.com>
2019-10-30 11:55:51 -0400

    Update index.php

--- webapp/index.php
+++ webapp/index.php
@@ -1,36 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$url = "http://35.173.247.206/api/tasks.php"
 
-$dbconnecterror = FALSE;
-$dbh = NULL;
-$dbReadError = FALSE;
-
-require_once 'credentials.php';
-
-try{
-	
-	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
-	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(Exception $e){
-	$dbconnecterror = TRUE;
-}
-
-if (!$dbconnecterror) {
-	
-	try {
-		$sql = "SELECT * FROM doList";
-		$stmt = $dbh->prepare($sql);
-		$stmt->execute();
-		$result = $stmt->fetchAll();
-	} catch (PDOException $e) {
-		$dbReadError = TRUE;
-	}
-
-}
+$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response  = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 ?>
 <!doctype html>
 <html lang="en">
