1391c4b9166acdac742417f35ffaba105ae0ac55
ryleegarrett <43964658+ryleegarrett@users.noreply.github.com>
2019-10-30 11:55:23 -0400

    Update edit.php

--- webapp/edit.php
+++ webapp/edit.php
@@ -1,63 +1,46 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-
-// Declare the credentials to the database
-$dbconnecterror = FALSE;
-$dbh = NULL;
-
-require_once 'credentials.php';
-
-try{
-	
-	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
-	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-}catch(Exception $e){
-	$dbconnecterror = TRUE;
-}
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	
-	$listID = $_POST['listID'];
-	
-	if (array_key_exists('fin', $_POST)) {
-		$complete = 1;
-	} else {
-		$complete = 0;
-	}
-	if (empty($_POST['finBy'])) {
-		$finBy = null;
-	} else {
-		$finBy = $_POST['finBy'];
-	}
-	$listItem = $_POST['listItem'];
-	
+    
+    $listID = $_POST['listID'];
+    
+    if (array_key_exists('fin', $_POST)) {
+        $complete = 1;
+    } else {
+        $complete = 0;
+    }
+    if (empty($_POST['finBy'])) {
+        $finBy = null;
+    } else {
+        $finBy = $_POST['finBy'];
+    }
+    $listItem = $_POST['listItem'];
+    
+    
+    $url = "http://35.173.247.206/api/task.php?listID=$listID";
+    
+    //create json string
+    $data = json_encode([
+        'taskName' => $listItem,
+        'taskDate' => $finBy,
+        'completed' => $complete
+    ]);
 
-	if (!$dbconnecterror) {
-		try {
-			$sql = "UPDATE doList SET complete=:complete, listItem=:listItem, finishDate=:finishDate WHERE listID=:listID";
-			$stmt = $dbh->prepare($sql);			
-			$stmt->bindParam(":complete", $complete);
-			$stmt->bindParam(":listItem", $listItem);
-			$stmt->bindParam(":finishDate", $finBy);
-			$stmt->bindParam(":listID", $listID);
+    //Define Curl request
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response  = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-			$response = $stmt->execute();	
-			
-			header("Location: index.php");
-			
-		} catch (PDOException $e) {
-			header("Location: index.php?error=edit");
-			
-		}	
-	} else {
-		header("Location: index.php?error=edit");
-	}
-}
+    if($httpcode == 204) { //Status code 204
+        header("Location: index.php");
+    } else { //Status code not 204
+        header("Location: index.php?error=edit");
+        }
+    }
 ?>
