bf1b65f36d202f490b3aff516ba94be63174a431
ryleegarrett <43964658+ryleegarrett@users.noreply.github.com>
2019-10-30 11:54:54 -0400

    Update delete.php

--- webapp/delete.php
+++ webapp/delete.php
@@ -1,44 +1,19 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-
-// Declare the credentials to the database
-$dbconnecterror = FALSE;
-$dbh = NULL;
-
-
-require_once 'credentials.php';
-
-
-try{
-	
-	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
-	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(Exception $e){
-	$dbconnecterror = TRUE;
-}
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	$url = "http://35.173.247.206/api/task.php?listID={$_POST['listID'])";
 
-	if (!$dbconnecterror) {
-		try {
-			$sql = "DELETE FROM doList where listID = :listID";
-			$stmt = $dbh->prepare($sql);			
-			$stmt->bindParam(":listID", $_POST['listID']);
-		
-			$response = $stmt->execute();	
-			
-			header("Location: index.php");
-			
-		} catch (PDOException $e) {
-			header("Location: index.php?error=delete");
-		}	
-	} else {
+	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response  = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpcode == 204) {
+    	header("Location: index.php");
+    }	else {
 		header("Location: index.php?error=delete");
 	}
 }
